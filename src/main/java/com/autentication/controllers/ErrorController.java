7eeb27c74b3fefe5eb04dc88907cc7e4db2e6644
Hernandes Andrade <hernandesandrade12@gmail.com>
2025-05-04 20:45:24 -0300

    Pagina de erro global criada

---
+++ src/main/java/com/autentication/controllers/ErrorController.java
@@ -0,0 +1,17 @@
+package com.autentication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        return "erro"; // retorna para templates/error.html
+    }
+}
+
